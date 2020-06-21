0090db5d9c8a758aff0b77b7bf6cfd347eee8e76
Celeste Glavin <cglavin@frame.io>
2020-06-21 12:47:26 -0400

    add some js

--- index.js
+++ index.js
@@ -2,4 +2,17 @@
   console.log("heyyyy");
 
   document.body.classList.add("blue");
+
+  const inputEl = document.getElementById("search");
+
+  inputEl.addEventListener("input", (event) => {
+    console.log("event.target.value", event.target.value);
+    fetch(
+      `https://api.github.com/search/users?q=${
+        event.target.value
+      }&per_page=${10}`
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  });
 })();
